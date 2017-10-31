c343c8dedc9dfc5c7f4d1b9bd156356cd8c0dcf4
Shun Hasegawa <pazeshun3684@gmail.com>
2017-10-31 17:53:52 +0900

    Use mraa library instead of wrong SPI communication

--- sphand_driver/src/gripper_v7_realtime_loop.cpp
+++ sphand_driver/src/gripper_v7_realtime_loop.cpp
@@ -1,6 +1,7 @@
 #include <fcntl.h>
 #include <sys/ioctl.h>
 #include <linux/spi/spidev.h>
+#include "mraa.hpp"
 
 #include <vector>
 #include <map>
@@ -10,149 +11,10 @@
 #include <hardware_interface/robot_hw.h>
 #include <controller_manager/controller_manager.h>
 
-class SPICommunication
-{
-private:
-  std::string file_;
-  int fd_;
-  uint32_t max_speed_;
-  uint8_t bits_per_word_;
-  uint8_t mode_;
-  uint16_t delay_;
-
-public:
-  SPICommunication(const std::string& file = "", const uint32_t max_speed = 500000, const uint8_t bits_per_word = 8,
-                   const uint8_t mode = 0, const uint16_t delay = 0)
-    : file_(file), max_speed_(max_speed), bits_per_word_(bits_per_word), mode_(mode), delay_(delay), fd_(-1)
-  {
-  }
-
-  ~SPICommunication()
-  {
-    end();
-  }
-
-  bool start(const std::string& file)
-  {
-    file_ = file;
-    return start();
-  }
-
-  bool start()
-  {
-    if (fd_ > 0)
-    {
-      ROS_WARN("SPI port already opens");
-      return true;
-    }
-
-    fd_ = open(file_.c_str(), O_RDWR);
-    if (fd_ < 0)
-    {
-      ROS_ERROR("Cannot open SPI port: %s", file_.c_str());
-      return false;
-    }
-
-    // max speed hz
-    int ret = ioctl(fd_, SPI_IOC_WR_MAX_SPEED_HZ, &max_speed_);
-    if (ret == -1)
-    {
-      ROS_ERROR("Cannot set max speed hz: %s", file_.c_str());
-      return false;
-    }
-
-    // bits per word
-    ret = ioctl(fd_, SPI_IOC_WR_BITS_PER_WORD, &bits_per_word_);
-    if (ret == -1)
-    {
-      ROS_ERROR("Cannot set bits per word: %s", file_.c_str());
-      return false;
-    }
-
-    // SPI mode
-    ret = ioctl(fd_, SPI_IOC_WR_MODE, &mode_);
-    if (ret == -1)
-    {
-      ROS_ERROR("Cannot set SPI mode: %s", file_.c_str());
-      return false;
-    }
-
-    return true;
-  }
-
-  bool getSPIInfo(std::string* file, uint32_t* max_speed, uint8_t* bits_per_word, uint8_t* mode, uint16_t* delay)
-  {
-    *file = file_;
-    *delay = delay_;
-
-    // max speed hz
-    int ret = ioctl(fd_, SPI_IOC_RD_MAX_SPEED_HZ, &max_speed_);
-    if (ret == -1)
-    {
-      ROS_ERROR("Cannot get max speed hz: %s", file_.c_str());
-      return false;
-    }
-    *max_speed = max_speed_;
-
-    // bits per word
-    ret = ioctl(fd_, SPI_IOC_RD_BITS_PER_WORD, &bits_per_word_);
-    if (ret == -1)
-    {
-      ROS_ERROR("Cannot get bits per word: %s", file_.c_str());
-      return false;
-    }
-    *bits_per_word = bits_per_word_;
-
-    // SPI mode
-    ret = ioctl(fd_, SPI_IOC_RD_MODE, &mode_);
-    if (ret == -1)
-    {
-      ROS_ERROR("Cannot get SPI mode: %s", file_.c_str());
-      return false;
-    }
-    *mode = mode_;
-
-    return true;
-  }
-
-  uint8_t transfer(const uint8_t data)
-  {
-    uint8_t tx[1] = {0};
-    uint8_t rx[1] = {0};
-    struct spi_ioc_transfer tr[1];
-
-    tx[0] = data;
-    tr[0].tx_buf = (unsigned long)tx;
-    tr[0].rx_buf = (unsigned long)rx;
-    tr[0].len = 1;
-    tr[0].delay_usecs = delay_;
-    tr[0].speed_hz = max_speed_;
-    tr[0].bits_per_word = bits_per_word_;
-
-    int ret = ioctl(fd_, SPI_IOC_MESSAGE(1), &tr);
-    if (ret < 1)
-    {
-      ROS_ERROR("Cannot send SPI message: %s", file_.c_str());
-      return 0;
-    }
-
-    return rx[0];
-  }
-
-  void end()
-  {
-    if (fd_ > 0)
-    {
-      close(fd_);
-      fd_ = -1;
-    }
-  }
-};  // end class SPICommunication
-
 class PressureSensorDriver
 {
 private:
-  SPICommunication spi_;
+  mraa::Spi spi_;
   uint16_t dig_T1_;
   int16_t dig_T2_;
   int16_t dig_T3_;
@@ -167,9 +29,10 @@ private:
   int16_t dig_P9_;
 
 public:
-  PressureSensorDriver(const std::string& file = "/dev/spidev2.0", const uint32_t max_speed = 8000000)
-    : spi_(file, max_speed, 8)
+  PressureSensorDriver(const int spi_bus = 2, const int spi_cs = 0, const uint32_t max_speed = 8000000)
+    : spi_(spi_bus, spi_cs)
   {
+    spi_.frequency(max_speed);
   }
 
   ~PressureSensorDriver()
@@ -178,41 +41,38 @@ public:
 
   void initBME()
   {
-    spi_.transfer((0xF5 & 0x7F));
-    spi_.transfer(0x20);
-    spi_.transfer((0xF4 & 0x7F));
-    spi_.transfer(0x27);
+    uint8_t tx[2];
+    tx[0] = 0xF5 & 0x7F;
+    tx[1] = 0x20;
+    spi_.write(tx, 2);
+    tx[0] = 0xF4 & 0x7F;
+    tx[1] = 0x27;
+    spi_.write(tx, 2);
   }
 
   void readTrim()
   {
-    uint8_t data[24];
-    spi_.transfer((0x88 | 0x80));
-    for (int i = 0; i < 24; i++)
-    {
-      data[i] = spi_.transfer(0);
-    }
+    uint8_t tx[25] = {};
+    uint8_t rx[25];
+    tx[0] = 0x88 | 0x80;
+    spi_.transfer(tx, rx, 25);
 
-    dig_T1_ = (data[1] << 8) | data[0];
-    dig_T2_ = (data[3] << 8) | data[2];
-    dig_T3_ = (data[5] << 8) | data[4];
-    dig_P1_ = (data[7] << 8) | data[6];
-    dig_P2_ = (data[9] << 8) | data[8];
-    dig_P3_ = (data[11] << 8) | data[10];
-    dig_P4_ = (data[13] << 8) | data[12];
-    dig_P5_ = (data[15] << 8) | data[14];
-    dig_P6_ = (data[17] << 8) | data[16];
-    dig_P7_ = (data[19] << 8) | data[18];
-    dig_P8_ = (data[21] << 8) | data[20];
-    dig_P9_ = (data[23] << 8) | data[22];
+    dig_T1_ = (rx[2] << 8) | rx[1];
+    dig_T2_ = (rx[4] << 8) | rx[3];
+    dig_T3_ = (rx[6] << 8) | rx[5];
+    dig_P1_ = (rx[8] << 8) | rx[7];
+    dig_P2_ = (rx[10] << 8) | rx[9];
+    dig_P3_ = (rx[12] << 8) | rx[11];
+    dig_P4_ = (rx[14] << 8) | rx[13];
+    dig_P5_ = (rx[16] << 8) | rx[15];
+    dig_P6_ = (rx[18] << 8) | rx[17];
+    dig_P7_ = (rx[20] << 8) | rx[19];
+    dig_P8_ = (rx[22] << 8) | rx[21];
+    dig_P9_ = (rx[24] << 8) | rx[23];
   }
 
   bool init()
   {
-    if (!spi_.start())
-    {
-      return false;
-    }
     initBME();
     readTrim();
 
@@ -222,17 +82,16 @@ public:
   void readRawPressureAndTemperature(uint32_t* pres_raw, uint32_t* temp_raw)
   {
     uint8_t data[8];
-    spi_.transfer((0xF7 | 0x80));
-    for (int i = 0; i < 8; i++)
-    {
-      data[i] = spi_.transfer(0x00);
-    }
-    *pres_raw = data[0];
-    *pres_raw = ((*pres_raw) << 8) | data[1];
-    *pres_raw = ((*pres_raw) << 4) | (data[2] >> 4);
-    *temp_raw = data[3];
-    *temp_raw = ((*temp_raw) << 8) | data[4];
-    *temp_raw = ((*temp_raw) << 4) | (data[5] >> 4);
+    uint8_t tx[9] = {};
+    uint8_t rx[9];
+    tx[0] = 0xF7 | 0x80;
+    spi_.transfer(tx, rx, 9);
+    *pres_raw = rx[1];
+    *pres_raw = ((*pres_raw) << 8) | rx[2];
+    *pres_raw = ((*pres_raw) << 4) | (rx[3] >> 4);
+    *temp_raw = rx[4];
+    *temp_raw = ((*temp_raw) << 8) | rx[5];
+    *temp_raw = ((*temp_raw) << 4) | (rx[6] >> 4);
   }
 
   int32_t calibTemperature(int32_t temp_raw)
