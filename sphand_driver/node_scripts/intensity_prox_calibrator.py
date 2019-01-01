907ba5588d3f16eaffd6b3f64be062636ef45ebd
Shun Hasegawa <pazeshun3684@gmail.com>
2019-01-01 14:57:48 +0900

    Use ProximityStampedArray in intensity_prox_calibrator.py

--- sphand_driver/node_scripts/intensity_prox_calibrator.py
+++ sphand_driver/node_scripts/intensity_prox_calibrator.py
@@ -2,9 +2,9 @@
 
 import numpy as np
 
-from force_proximity_ros.msg import ProximityArray
 from sphand_driver.msg import IntensityProxCalibInfo
 from sphand_driver.msg import IntensityProxCalibInfoArray
+from sphand_driver.msg import ProximityStampedArray
 from vl53l0x_mraa_ros.msg import RangingMeasurementDataStampedArray
 from std_srvs.srv import Trigger
 from std_srvs.srv import TriggerResponse
@@ -27,7 +27,7 @@ class IntensityProxCalibrator(object):
             self.i_height_from_tof = np.array(self.i_height_from_tof)
         self.tof_valid_min = rospy.get_param('~tof_valid_min', 30)
         self.i_tof_tm_tolerance = rospy.get_param('~i_tof_tm_tolerance', 0.02)
-        self.i_last_tm = None
+        self.i_last_tms = None
         self.i_raw = None
         self.i_diff_from_init = None
 
@@ -35,15 +35,15 @@ class IntensityProxCalibrator(object):
             '~output', IntensityProxCalibInfoArray,
             queue_size=1)
         self.sub_input_i = rospy.Subscriber(
-            '~input/intensity', ProximityArray, self._intensity_cb)
+            '~input/intensity', ProximityStampedArray, self._intensity_cb)
         self.sub_input_tof = rospy.Subscriber(
             '~input/tof', RangingMeasurementDataStampedArray, self._tof_cb)
         self.init_srv = rospy.Service(
             '~set_init_proximities', Trigger, self._set_init_proximities)
 
     def _intensity_cb(self, msg):
-        self.i_last_tm = msg.header.stamp
-        self.i_raw = np.array([p.proximity for p in msg.proximities])
+        self.i_last_tms = [p.header.stamp for p in msg.proximities]
+        self.i_raw = np.array([p.proximity.proximity for p in msg.proximities])
         if self.i_init is None:
             rospy.logwarn_throttle(10, 'Init prox is not set, so skipping')
             return
@@ -62,9 +62,13 @@ class IntensityProxCalibrator(object):
         # Publish calibrated info
         pub_msg = IntensityProxCalibInfoArray()
         pub_msg.header.stamp = msg.header.stamp
-        for dist, diff, const, init in zip(distance, self.i_diff_from_init,
-                                           self.i_prop_const, self.i_init):
+        for p, dist, diff, const, init in zip(msg.proximities,
+                                              distance,
+                                              self.i_diff_from_init,
+                                              self.i_prop_const,
+                                              self.i_init):
             info = IntensityProxCalibInfo()
+            info.header.stamp = p.header.stamp
             info.distance = dist
             info.diff_from_init = diff
             info.prop_const = const
@@ -78,12 +82,13 @@ class IntensityProxCalibrator(object):
                 10, 'Prox diff_from_init is not set, so skipping')
             return
         assert len(self.i_diff_from_init) == len(msg.array)
+        assert len(msg.array) == len(self.i_last_tms)
         if self.i_height_from_tof is None:
             self.i_height_from_tof = [0.0] * len(msg.array)
         if self.i_prop_const is None:
             self.i_prop_const = np.zeros(len(msg.array))
         for i, data_st in enumerate(msg.array):
-            if abs((data_st.header.stamp - self.i_last_tm).to_sec()) > \
+            if abs((data_st.header.stamp - self.i_last_tms[i]).to_sec()) > \
                self.i_tof_tm_tolerance:
                 continue
             if self.i_diff_from_init[i] < self.i_valid_min:
